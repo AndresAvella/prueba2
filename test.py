422934168019b7c53a469504d46b2e07e8fa22c6
AndresAvella <ra.avella10@uniandes.edu.co>
2018-11-21 18:30:13 +0000

    mundo cruel dejame comentar

--- test.py
+++ test.py
@@ -4,4 +4,20 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-print("hola mundo cruel")
+print("hola mundo cruel , muy cruel ")
+i=1
+while i <200:
+    if(i==1):
+        print ( i, " mundo muy ")
+    if(i>2):
+        print("muuuuuyy")
+    if(i==199):
+                print("cruel")
+        
+    i=i+1
+    
+plt.figure()
+x=np.linspace(0,100)
+y=np.linspace(0,100)
+plt.plot(x,y)
+plt.savefig("mundo cruel")
